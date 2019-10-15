55e95e745737286cba2926530d2bfa6bb15543c7
supakorns <tonytonychopss@gmail.com>
2019-10-15 06:08:16 -0400

    jenkins.bat-4th

--- jenkins.bat
+++ jenkins.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
-xcopy C:\Jenkins_Workspaces\workspace\clone_and_copy C:\Users\Administrator\Desktop\1234 /t /e /i /y
+xcopy C:\Jenkins_Workspaces\workspace\clone_and_copy C:\Users\Administrator\Desktop\1234 /c /d /e /h /i /k /q /r /s /x /y
 PAUSE          
